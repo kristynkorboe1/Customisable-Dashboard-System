af7d3470db64b4075464f9885289763c0d959b94
Kristyn Korboe <s194704@student.dtu.dk>
2023-01-12 02:02:03 +0100

    Updates to API

--- server/routes/api/patientData.js
+++ server/routes/api/patientData.js
@@ -35,15 +35,23 @@ async function loadCarbohydrateDataCollection() {
     return client.db('customisableDashboardSystem').collection('carbohydrate')
 }
 
+async function loadGlucoseDataCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://dbuser:y2J4VJdzSu53eJUV@cluster0.luieljo.mongodb.net/test', {
+        useNewUrlParser: true
+    })
+
+    return client.db('customisableDashboardSystem').collection('glucose')
+}
+
 
 //Get all basal insulin data
 router.get('/basalInsulin', async (req, res) => {
     try {
         const basalInsulinData = await loadBasalInsulinDataCollection();
-        res.send(await basalInsulinData.find({}).toArray());
+        res.status(200).send(await basalInsulinData.find({}).toArray());
     }
     catch(err) {
-    res.send({ message: err})
+        res.status(500).json({ error: 'Could not retrive basal insulin data'})
     }
 });
 
@@ -51,10 +59,10 @@ router.get('/basalInsulin', async (req, res) => {
 router.get('/bolusInsulin', async (req, res) => {
     try {
         const bolusInsulinData = await loadBolusInsulinDataCollection();
-        res.send(await bolusInsulinData.find({}).toArray());
+        res.status(200).send(await bolusInsulinData.find({}).toArray());
     }
     catch(err) {
-    res.send({ message: err})
+        res.status(500).json({ error: 'Could not retrive bolus insulin data'})
     }
 });
 
@@ -62,10 +70,10 @@ router.get('/bolusInsulin', async (req, res) => {
 router.get('/physicalActivity', async (req, res) => {
     try {
         const physicalActivityData = await loadPhysicalActivityDataCollection();
-        res.send(await physicalActivityData.find({}).toArray());
+        res.status(200).send(await physicalActivityData.find({}).toArray());
     }
     catch(err) {
-    res.send({ message: err})
+        res.status(500).json({ error: 'Could not retrive physical activity data'})
     }
 });
 
@@ -73,10 +81,21 @@ router.get('/physicalActivity', async (req, res) => {
 router.get('/carbohydrate', async (req, res) => {
     try {
         const carbohydrateData = await loadCarbohydrateDataCollection();
-        res.send(await carbohydrateData.find({}).toArray());
+        res.status(200).send(await carbohydrateData.find({}).toArray());
     }
     catch(err) {
-    res.send({ message: err})
+        res.status(500).send({ error: 'Could not retrive carbohydrate data'})
+    }
+});
+
+//Get all glucose data
+router.get('/glucose', async (req, res) => {
+    try {
+        const glucoseData = await loadGlucoseDataCollection();
+        res.status(200).send(await glucoseData.find({}).toArray());
+    }
+    catch(err) {
+        res.status(500).send({ error: 'Could not retrive glucose data'})
     }
 });
 
@@ -99,7 +118,7 @@ router.post('/basalInsulin', async (req,res) => {
         res.status(201).send()
     }   
     catch(err) {
-        res.json({message: err})
+        res.status(500).send({message: err})
     } 
 });
 
@@ -122,7 +141,7 @@ router.post('/bolusInsulin', async (req,res) => {
         res.status(201).send()
     }   
     catch(err) {
-        res.json({message: err})
+        res.status(500).send({message: err})
     } 
 });
 
@@ -131,19 +150,19 @@ router.post('/physicalActivity', async (req,res) => {
     try {
         const physicalActivityData = await loadPhysicalActivityDataCollection();
         const currentDate = new Date(); 
-        const date = 
-            currentDate.getDate() + "/"
-            + (currentDate.getMonth()+1)  + "/" 
-            + currentDate.getFullYear()
+        // const date = 
+        //     currentDate.getDate() + "/"
+        //     + (currentDate.getMonth()+1)  + "/" 
+        //     + currentDate.getFullYear()
 
         await physicalActivityData.insertOne({
-            time: date,
+            time: currentDate,
             physicalActivityMin: req.body.physicalActivityMin
         });
         res.status(201).send()
     }   
     catch(err) {
-        res.json({message: err})
+        res.status(500).send({message: err})
     } 
 });
 
@@ -152,21 +171,113 @@ router.post('/carbohydrate', async (req,res) => {
     try {
         const carbohydrateData = await loadCarbohydrateDataCollection();
         const currentDate = new Date(); 
-        const date = 
-            currentDate.getDate() + "/"
-            + (currentDate.getMonth()+1)  + "/" 
-            + currentDate.getFullYear()
+        // const date = 
+        //     currentDate.getDate() + "/"
+        //     + (currentDate.getMonth()+1)  + "/" 
+        //     + currentDate.getFullYear()
 
         await carbohydrateData.insertOne({
-            time: date,
+            time: currentDate,
             carbohydrateGrams: req.body.carbohydrateGrams
         });
         res.status(201).send()
     }   
     catch(err) {
-        res.json({message: err})
+        res.status(500).send({message: err})
     } 
 });
+
+//Add glucose data
+router.post('/glucose', async (req,res) => {
+    try {
+        const glucoseData = await loadGlucoseDataCollection();
+        const currentDate = new Date(); 
+        const dateTime = 
+            currentDate.getDate() + "/"
+            + (currentDate.getMonth()+1)  + "/" 
+            + currentDate.getFullYear() + " "  
+            + currentDate.getHours() + ":"  
+            + currentDate.getMinutes() + ":"
+
+        await glucoseData.insertOne({
+            time: dateTime,
+            glucoseGrams: req.body.glucoseGrams
+        });
+        res.status(201).send()
+    }   
+    catch(err) {
+        res.status(500).send({message: err})
+    } 
+});
+
+//Update physical activity data
+router.patch('/physicalActivity/:id', async (req, res) => {
+    try{
+        const updatedValue = req.body
+        const physicalActivityData = await loadPhysicalActivityDataCollection();
+        await physicalActivityData.updateOne({_id: new mongodb.ObjectId(req.params.id)}, {$set: updatedValue})
+        res.status(200).send();
+    }
+    catch(err){
+        res.status(500).send({message: err})
+    }
+})
+
+//Update carbohydrate data
+router.patch('/carbohydrate/:id', async (req, res) => {
+    try{
+        const updatedValue = req.body
+        const carbohydrateData = await loadCarbohydrateDataCollection();
+        await carbohydrateData.updateOne({_id: new mongodb.ObjectId(req.params.id)}, {$set: updatedValue})
+        res.status(200).send();
+    }
+    catch(err){
+        res.status(500).send({message: err})
+    }
+})
+
+//Get physical activity data from past week
+router.get('/physicalActivity/pastWeek', async (req, res) => {
+    try {
+        const lastWeekDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+        const physicalActivityData = await loadPhysicalActivityDataCollection();
+        res.status(200)
+            .send(await physicalActivityData
+                .find({time: {$gte: lastWeekDate}}).toArray());
+    }
+    catch(err) {
+        res.status(500).json({ error: 'Could not retrive physical activity data for the past week'})
+    }
+});
+
+//Get carbohydrate data from past week
+router.get('/carbohydrate/pastWeek', async (req, res) => {
+    try {
+        const lastWeekDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+        const carbohydrateData = await loadCarbohydrateDataCollection();
+        res.status(200)
+            .send(await carbohydrateData
+                .find({time: {$gte: lastWeekDate}}).toArray());
+    }
+    catch(err) {
+        res.status(500).json({ error: 'Could not retrive carbohydrate data for the past week'})
+    }
+});
+
+//Get TIR data - two weeks of glucose data
+router.get('/glucose/pastWeek', async (req, res) => {
+    try {
+        const lastWeekDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+        const glucoseData = await loadGlucoseDataCollection();
+        res.status(200)
+            .send(await glucoseData
+                .find({time: {$gte: lastWeekDate}}).toArray());
+    }
+    catch(err) {
+        res.status(500).json({ error: 'Could not retrive glucose data for the past week'})
+    }
+});
+
 
 //Delete insulin data
 // router.delete('/:id', async (req, res) => {
@@ -176,7 +287,7 @@ router.post('/carbohydrate', async (req,res) => {
 //         res.status(200).send();
 //     }
 //     catch(err){
-//         res.json({message: err})
+//         res.send({message: err})
 //     }
 // });
 
