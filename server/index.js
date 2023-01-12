c3a623cb32bbcd39754120c4592218f15ab9fceb
Kristyn Korboe <s194704@student.dtu.dk>
2023-01-12 07:13:40 +0100

    Minor changes to index file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const cors = require('cors');
 
 const app = express();
 
 //Middleware
 app.use(bodyParser.json());
+// app.use(cors());
 
 const patientData = require('./routes/api/patientData');
 
