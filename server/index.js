c3a08e8b0e6768d510ab8d7c9f380047f4538d9d
Kristyn Korboe <s194704@student.dtu.dk>
2023-01-19 18:07:35 +0100

    Tidying up

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 
 const app = express();
 
-//Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -12,7 +11,5 @@ const patientData = require('./routes/api/patientData');
 
 app.use('/api/patientData', patientData);
 
-
-//Listen to the server
 const port = 8080;
 app.listen(port, () => console.log(`Server started on port ${port}`));
